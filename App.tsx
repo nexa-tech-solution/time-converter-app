89d381f01bb96f8cc3305c1fe1124580edf5dfd2
Tien25012003 <nguyenthiphuongtien12e@gmail.com>
2026-05-16 21:56:39 +0700

    feat: init app

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const APP_URL = 'https://time-converter-woad-iota.vercel.app/';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <WebView source={{ uri: APP_URL }} style={styles.webview} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default App;
